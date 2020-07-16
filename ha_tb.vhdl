2e38046b4ba290dad2c436f373d718801b7bf9f3
Mostafa Othman <smogeq7@yahoo.com>
2020-07-16 06:11:57 +0200

    change ms to ns

--- ha_tb.vhdl
+++ ha_tb.vhdl
@@ -22,23 +22,23 @@ begin
 
 		a <= 'X';
 		b <= 'X';
-		wait for 1 ms;
+		wait for 1 ns;
 		
 		a <= '0';
                 b <= '0';
-                wait for 1 ms;
+                wait for 1 ns;
 		
 		a <= '0';
                 b <= '1';
-                wait for 1 ms;
+                wait for 1 ns;
 		
 		a <= '1';
                 b <= '0';
-                wait for 1 ms;
+                wait for 1 ns;
 
 		a <= '1';
                 b <= '1';
-                wait for 1 ms;
+                wait for 1 ns;
 
 		assert false report "End Of Test";
 		wait;
